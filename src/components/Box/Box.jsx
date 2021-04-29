2bfe703b2222494db3ae8bfc57119488b0674000
Ennis Machta <esmachta@gmail.com>
2021-04-28 19:57:18 -0700

    latest changes

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./styles";
+
+function Box({ children }) {
+  return <Container>{children}</Container>;
+}
+
+export default Box;
---
+++ src/components/Box/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 348px;
+  height: 358px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-color: #dbdbdb;
+  border-width: 1px;
+  border-style: solid;
+`;
